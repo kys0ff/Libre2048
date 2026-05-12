78c4574cbbe50b1fa98eb23416b0d125f47125fc
kys0ff <idk683120@gmail.com>
2026-05-12 13:37:01 +0100

    cleanup project configuration and remove unused resources.

--- app/src/main/java/off/kys/libre2048/ui/theme/Theme.kt
+++ app/src/main/java/off/kys/libre2048/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package off.kys.libre2048.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
