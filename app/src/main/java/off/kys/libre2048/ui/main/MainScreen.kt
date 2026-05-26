3a57997647e314fc606ec87432bf28bda13bc3bc
kys0ff <idk683120@gmail.com>
2026-05-26 13:39:31 +0100

    add status bar padding and refactor info button to IconButton.

--- app/src/main/java/off/kys/libre2048/ui/main/MainScreen.kt
+++ app/src/main/java/off/kys/libre2048/ui/main/MainScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -25,6 +26,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -124,12 +126,11 @@ class MainScreen : Screen {
                     Box(
                         modifier = Modifier
                             .fillMaxWidth()
+                            .statusBarsPadding()
                             .padding(horizontal = dimens.topPadding, vertical = dimens.topPadding)
                     ) {
-                        Surface(
+                        IconButton(
                             onClick = { navigator += AboutScreen() },
-                            shape = CircleShape,
-                            color = MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.5f),
                             modifier = Modifier.align(Alignment.TopEnd)
                         ) {
                             Icon(
@@ -138,7 +139,6 @@ class MainScreen : Screen {
                                 modifier = Modifier
                                     .padding(dimens.infoIconPadding)
                                     .size(dimens.infoIconSize),
-                                tint = MaterialTheme.colorScheme.onSurfaceVariant
                             )
                         }
                     }
